e80cde9a8f9a38c38c8f57860ec5fb37ea5dfc61
Rami Sahili <rami@newscatcherapi.com>
2025-08-03 21:10:56 +0300

    remove jobs

--- dagster_project/repository.py
+++ dagster_project/repository.py
@@ -1,9 +1,7 @@
 from dagster import Definitions
-from jobs import spider_job
 from assets.trigger_api import trigger_transform_api
 from assets.trigger_spider import run_spider_asset
 
 defs = Definitions(
-    # jobs=[spider_job],
     assets=[trigger_transform_api, run_spider_asset],
 )
